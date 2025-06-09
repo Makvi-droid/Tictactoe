dac9cdc517361d8b8bbeae90ceb033f3e89c2803
Makvi-droid <pasbenauro05@gmail.com>
2025-06-09 22:34:45 +0800

    added functionalities(swapping turns and symbols appearing)

---
+++ script.js
@@ -0,0 +1,25 @@
+const cellElements = document.querySelectorAll('[data-cell]');
+
+const x_class = 'x';
+const circle_class = 'circle';
+
+let circleTurn;
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true})
+})
+
+function handleClick(e){
+    const cell = e.target;
+    const currentClass = circleTurn ? circle_class : x_class;
+    placeMark(cell, currentClass)
+    swapTurn();
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass);
+}
+
+function swapTurn(){
+    circleTurn = !circleTurn;
+}
